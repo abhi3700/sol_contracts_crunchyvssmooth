76e4134af15581a645b8672c11b93c9d6f228f87
Abhijit Roy <dalvath3700@gmail.com>
2021-12-02 19:19:37 +0530

    typescript test added

--- tests/crunchy-vs-smooth.ts
+++ tests/crunchy-vs-smooth.ts
@@ -1,17 +1,85 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { CrunchyVsSmooth } from '../target/types/crunchy_vs_smooth';
+const assert = require('assert');
+const { SystemProgram } = anchor.web3;
 
 describe('crunchy-vs-smooth', () => {
+  // let account: String;
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.CrunchyVsSmooth as Program<CrunchyVsSmooth>;
+  const voteAccount = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  beforeEach('Initializes with 0 votes for crunchy and smooth', async () => {
+      // console.log("Testing Initialize...");
+
+    /* The last element passed to RPC methods is always the transaction options. Because voteAccount is being created here, we are required to pass it as a signers array */
+    await program.rpc.initialize({
+      accounts: {
+        voteAccount: voteAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [voteAccount],
+    });
+    
+    // console.log("Your transaction signature", tx);
+
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+    // console.log(`Crunchy: ${account.crunchy.toString()}`);
+    // console.log(`Smooth: ${account.crunchy.toString()}`);
+
+    assert.ok(
+      account.crunchy.toString() == 0 && account.smooth.toString() == 0
+    );
+
   });
+
+  it("Votes correctly for crunchy", async () => {
+    // vote for crunchy
+    await program.rpc.voteCrunchy({
+      accounts: {
+        voteAccount: voteAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    // console.log("Crunchy: ", account.crunchy.toString());
+    // console.log("Smooth: ", account.smooth.toString());
+    // assert that the vote for crunchy == 1
+    assert.ok(
+      account.crunchy.toString() == 1 && account.smooth.toString() == 0
+    );
+  });
+
+  it("Votes correctly for smooth", async () => {
+    // vote for smooth
+    await program.rpc.voteSmooth({
+      accounts: {
+        voteAccount: voteAccount.publicKey,
+      }
+    });
+
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    // console.log("Crunchy: ", account.crunchy.toString());
+    // console.log("Smooth: ", account.smooth.toString());
+
+    // assert that the vote for smooth == 1
+    assert.ok(
+      account.crunchy.toString() == 0 && account.smooth.toString() == 1
+    );
+  });
+
 });
